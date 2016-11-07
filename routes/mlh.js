ce6715c3e17c740798487eb9b2ce52ba65200f43
John Bucknam <jbuck594@gmail.com>
2016-11-06 21:54:14 -0500

    Adding Secret Key for testing
    To be reset on publishing

--- routes/mlh.js
+++ routes/mlh.js
@@ -6,7 +6,7 @@ var router  = express.Router();
 
 var mlhAuth = new ClientOAuth2({
   clientId: "fc30a89abaa8a953a070443edcb5317c3d7d27c0866104a60420e687f4f75cce",
-  clientSecret: process.env.SECRET_KEY,
+  clientSecret: process.env.SECRET_KEY || "b5e5223a4d18ddca1c8a914a70f10e0d56bc35757c0ab0e7142e743887724e57",
   accessTokenUri: "https://my.mlh.io/oauth/token",
   authorizationUri: "https://my.mlh.io/oauth/authorize",
   redirectUri: "http://localhost:3000/mlh/callback",
