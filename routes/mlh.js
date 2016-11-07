f2b72cf68112eba6ad6049c26c153ccae7314b6d
John Bucknam <jbuck594@gmail.com>
2016-11-06 19:47:05 -0500

    Fixed semicolon

--- routes/mlh.js
+++ routes/mlh.js
@@ -7,7 +7,7 @@ router.get('/auth', function(req, res, next) {
   /*res.redirect("https://my.mlh.io/oauth/authorize?client_id=fc30a89abaa8a953a070443edcb5317c3d7d27c0866104a60420e687f4f75cce" +
     "&redirect_uri=http://localhost:3000/mlh/token&response_type=code&scope=email+education+birthday");*/
   res.redirect("https://my.mlh.io/oauth/authorize?client_id=fc30a89abaa8a953a070443edcb5317c3d7d27c0866104a60420e687f4f75cce" +
-    "&redirect_uri=http://localhost:3000/form.html&response_type=token&scope=email+education+birthday")
+    "&redirect_uri=http://localhost:3000/form.html&response_type=token&scope=email+education+birthday");
   res.end();
 });
 
